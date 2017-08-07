a4eac49d1c650feec56627a240966f3f762c7ce4
Agith Yogendra <agithyogendra@gmail.com>
2017-08-06 21:27:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
 
 	function getLocation(position){
 
-		$.get("http://ipinfo.io", function (response) {
+		$.get("https://cors-anywhere.herokuapp.com/http://ipinfo.io", function (response) {
     	$("#city").html(response.city + ", " + response.region);
     	}, "jsonp");
 
